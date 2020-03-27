c7b9119f278d49b4c27ef1fe529f273222beb9d2
UchihaPatrickStar <newbiechen@foxmail.com>
2020-03-27 13:11:24 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 禁用方法名圆括号前补空格的语法规则
+    'space-before-function-paren': 0
   }
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message // 通过全局配置各组件可通过this.$message调用弹框组件

7bac7de89649b8eb6c7d5055a7c7c0fbd4c72446
UchihaPatrickStar <newbiechen@foxmail.com>
2020-03-31 22:18:44 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -36,7 +36,9 @@ import {
   Step,
   CheckboxGroup,
   Checkbox,
-  Upload
+  Upload,
+  Timeline,
+  TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -74,5 +76,7 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$message = Message // 通过全局配置各组件可通过this.$message调用弹框组件
 Vue.prototype.$confirm = MessageBox.confirm
